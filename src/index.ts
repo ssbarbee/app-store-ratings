082330e24cdc6207dab12e6c3892a30f9d43e093
ssbarbee <ssbarbee@gmail.com>
2019-12-15 21:11:11 +0100

    Update types.

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,8 @@
 import axios from "axios";
 
 import { parseStringPromise } from "xml2js";
-import AppStoreFeed = namespace.AppStoreFeed;
-import Entry = namespace.Entry;
+import { IAppStoreFeed, IEntry, IFetchRatingsFilter, IProjectConfig } from "./types";
 
-interface IProjectConfig {
-  projectId: string;
-  country?: string;
-}
-interface IFetchRatingsFilter {
-  fromDate?: Date;
-}
 const isEmptyString = (str?: string): boolean => !str || str.length === 0;
 const constructUrl = (config: IProjectConfig): string => {
   if (config.country) {
@@ -18,21 +10,21 @@ const constructUrl = (config: IProjectConfig): string => {
   }
   return `https://itunes.apple.com/rss/customerreviews/id=${config.projectId}/sortBy=mostRecent/xml`;
 };
-const filterEntries = (entries: Entry[], filter?: IFetchRatingsFilter): Entry[] => {
+const filterEntries = (entries: IEntry[], filter?: IFetchRatingsFilter): IEntry[] => {
   if (!filter) {
     return entries;
   }
   return entries.filter(e => e.updated);
 };
 
-export const fetchRatings = async (projectConfig: IProjectConfig, filter?: IFetchRatingsFilter): Promise<Entry[]> => {
+export const fetchRatings = async (projectConfig: IProjectConfig, filter?: IFetchRatingsFilter): Promise<IEntry[]> => {
   if (isEmptyString(projectConfig.projectId)) {
     throw new Error("projectId must be a non empty string");
   }
   const ratingsUrl = constructUrl(projectConfig);
   const response = await axios.get(ratingsUrl);
   try {
-    const json: AppStoreFeed = await parseStringPromise(response.data);
+    const json: IAppStoreFeed = await parseStringPromise(response.data);
     return filterEntries(json.feed.entry, filter);
   } catch (err) {
     throw new Error(err);
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,86 +1,92 @@
-declare module namespace {
+export interface ILink {
+  _rel: string;
+  _type: string;
+  _href: string;
+}
 
-  export interface Link {
-    _rel: string;
-    _type: string;
-    _href: string;
-  }
+export interface IAuthor {
+  name: string;
+  uri: string;
+}
 
-  export interface Author {
-    name: string;
-    uri: string;
-  }
+export interface IContent {
+  _type: string;
+  __text: string;
+}
 
-  export interface Content {
-    _type: string;
-    __text: string;
-  }
+export interface IContentType {
+  _term: string;
+  _label: string;
+  __prefix: string;
+}
 
-  export interface ContentType {
-    _term: string;
-    _label: string;
-    __prefix: string;
-  }
+export interface IVoteSum {
+  __prefix: string;
+  __text: string;
+}
 
-  export interface VoteSum {
-    __prefix: string;
-    __text: string;
-  }
+export interface IVoteCount {
+  __prefix: string;
+  __text: string;
+}
 
-  export interface VoteCount {
-    __prefix: string;
-    __text: string;
-  }
+export interface IRating {
+  __prefix: string;
+  __text: string;
+}
 
-  export interface Rating {
-    __prefix: string;
-    __text: string;
-  }
+export interface IVersion {
+  __prefix: string;
+  __text: string;
+}
 
-  export interface Version {
-    __prefix: string;
-    __text: string;
-  }
+export interface IAuthor2 {
+  name: string;
+  uri: string;
+}
 
-  export interface Author2 {
-    name: string;
-    uri: string;
-  }
+export interface ILink2 {
+  _rel: string;
+  _href: string;
+}
 
-  export interface Link2 {
-    _rel: string;
-    _href: string;
-  }
+export interface IEntry {
+  updated: Date;
+  id: string;
+  title: string;
+  content: IContent[];
+  contentType: IContentType;
+  voteSum: IVoteSum;
+  voteCount: IVoteCount;
+  rating: IRating;
+  version: IVersion;
+  author: IAuthor2;
+  link: ILink2;
+}
 
-  export interface Entry {
-    updated: Date;
-    id: string;
-    title: string;
-    content: Content[];
-    contentType: ContentType;
-    voteSum: VoteSum;
-    voteCount: VoteCount;
-    rating: Rating;
-    version: Version;
-    author: Author2;
-    link: Link2;
-  }
+export interface IFeed {
+  id: string;
+  title: string;
+  updated: Date;
+  link: ILink[];
+  icon: string;
+  author: IAuthor;
+  rights: string;
+  entry: IEntry[];
+  "_xmlns:im": string;
+  _xmlns: string;
+  "_xml:lang": string;
+}
 
-  export interface Feed {
-    id: string;
-    title: string;
-    updated: Date;
-    link: Link[];
-    icon: string;
-    author: Author;
-    rights: string;
-    entry: Entry[];
-    "_xmlns:im": string;
-    _xmlns: string;
-    "_xml:lang": string;
-  }
+export interface IAppStoreFeed {
+  feed: IFeed;
+}
 
-  export interface AppStoreFeed {
-    feed: Feed;
-  }
+export interface IProjectConfig {
+  projectId: string;
+  country?: string;
+}
+
+export interface IFetchRatingsFilter {
+  fromDate?: Date;
 }
