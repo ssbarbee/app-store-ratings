2b040de43566ffdce5380a0d2a5e4d0a972cb42f
ssbarbee <ssbarbee@gmail.com>
2019-12-16 18:56:24 +0100

    Types cleanup.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 import { parseStringPromise } from "xml2js";
-import { IAppStoreFeed, IEntry, IFetchRatingsFilter, IProjectConfig } from "./types";
+import { IAppStoreFeed, IAppStoreRating, IEntry, IProjectConfig } from "./types";
 
 const isEmptyString = (str?: string): boolean => !str || str.length === 0;
 const constructUrl = (config: IProjectConfig): string => {
@@ -10,14 +10,22 @@ const constructUrl = (config: IProjectConfig): string => {
   }
   return `https://itunes.apple.com/rss/customerreviews/id=${config.projectId}/sortBy=mostRecent/xml`;
 };
-const filterEntries = (entries: IEntry[], filter?: IFetchRatingsFilter): IEntry[] => {
-  if (!filter) {
-    return entries;
-  }
-  return entries.filter(e => e.updated);
-};
+const mapEntries = (entry: IEntry): IAppStoreRating => ({
+  author: {
+    name: entry.author[0].name[0],
+    uri: entry.author[0].uri[0],
+  },
+  content: entry.content[0]._,
+  id: entry.id[0],
+  rating: parseInt(entry['im:rating'][0], 10),
+  title: entry.title[0],
+  updatedAt: entry.updated[0],
+  version: entry['im:version'][0],
+  voteCount: parseInt(entry['im:voteCount'][0], 10),
+  voteSum: parseInt(entry['im:voteSum'][0], 10),
+});
 
-export const fetchRatings = async (projectConfig: IProjectConfig, filter?: IFetchRatingsFilter): Promise<IEntry[]> => {
+export const fetchRatings = async (projectConfig: IProjectConfig): Promise<IAppStoreRating[]> => {
   if (isEmptyString(projectConfig.projectId)) {
     throw new Error("projectId must be a non empty string");
   }
@@ -25,7 +33,10 @@ export const fetchRatings = async (projectConfig: IProjectConfig, filter?: IFetc
   const response = await axios.get(ratingsUrl);
   try {
     const json: IAppStoreFeed = await parseStringPromise(response.data);
-    return filterEntries(json.feed.entry, filter);
+    if(!json.feed || !json.feed.entry) {
+      return [];
+    }
+    return json.feed.entry.map(mapEntries);
   } catch (err) {
     throw new Error(err);
   }
--- src/types.ts
+++ src/types.ts
@@ -1,92 +1,42 @@
-export interface ILink {
-  _rel: string;
-  _type: string;
-  _href: string;
-}
-
-export interface IAuthor {
-  name: string;
-  uri: string;
-}
-
-export interface IContent {
-  _type: string;
-  __text: string;
-}
-
-export interface IContentType {
-  _term: string;
-  _label: string;
-  __prefix: string;
-}
-
-export interface IVoteSum {
-  __prefix: string;
-  __text: string;
-}
-
-export interface IVoteCount {
-  __prefix: string;
-  __text: string;
-}
-
-export interface IRating {
-  __prefix: string;
-  __text: string;
-}
-
-export interface IVersion {
-  __prefix: string;
-  __text: string;
-}
-
-export interface IAuthor2 {
-  name: string;
-  uri: string;
-}
-
-export interface ILink2 {
-  _rel: string;
-  _href: string;
-}
-
 export interface IEntry {
-  updated: Date;
-  id: string;
-  title: string;
-  content: IContent[];
-  contentType: IContentType;
-  voteSum: IVoteSum;
-  voteCount: IVoteCount;
-  rating: IRating;
-  version: IVersion;
-  author: IAuthor2;
-  link: ILink2;
+  updated: string[];
+  id: string[];
+  title: string[];
+  content: Array<{
+    _: string;
+  }>;
+  'im:voteCount': string[];
+  'im:voteSum': string[];
+  'im:rating': string[];
+  'im:version': string[];
+  author: Array<{
+    name: string;
+    uri: string;
+  }>;
 }
 
-export interface IFeed {
+export interface IAppStoreRating {
   id: string;
   title: string;
-  updated: Date;
-  link: ILink[];
-  icon: string;
-  author: IAuthor;
-  rights: string;
-  entry: IEntry[];
-  "_xmlns:im": string;
-  _xmlns: string;
-  "_xml:lang": string;
+  updatedAt: string;
+  content: string;
+  rating: number;
+  voteCount: number;
+  voteSum: number;
+  version: string;
+  author: {
+    name: string;
+    uri: string;
+  }
 }
 
 export interface IAppStoreFeed {
-  feed: IFeed;
+  feed: {
+    entry: IEntry[]
+  };
 }
 
 export interface IProjectConfig {
   projectId: string;
   country?: string;
-}
-
-export interface IFetchRatingsFilter {
-  fromDate?: Date;
 }
