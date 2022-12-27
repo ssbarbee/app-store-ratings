4d6299d61bc0abb0ceef083fb7e7e73cf08a8b74
ssbarbee <ssbarbee@gmail.com>
2022-12-27 23:30:56 +0100

    refactor: prettier

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -5,12 +5,12 @@ describe('app-store-ratings', function () {
   it('should throw error when no project specified', async () => {
     try {
       await fetchRatings({
-        projectId: ''
-      })
-    } catch(err) {
+        projectId: '',
+      });
+    } catch (err) {
       expect(err).not.to.equal(undefined);
       expect(err).not.to.equal(null);
-      expect((err as Error).message).to.equal('projectId must be a non empty string')
+      expect((err as Error).message).to.equal('projectId must be a non empty string');
     }
   });
 });
